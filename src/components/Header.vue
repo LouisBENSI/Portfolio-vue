4e653e1e834d62c44b2adaba0994c4f42a2bd46e
Locc35 <bensilouis@live.fr>
2022-11-23 23:45:05 +0100

    fix night mode

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,12 +7,12 @@
         </div>
         <div class="relative z-50 md:hidden">
           <!--BURGER-->
-          <svg xmlns="http://www.w3.org/2000/svg" @click="apparitionHeader()" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" @click="apparitionHeader()" class="h-10 w-10 burger" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"/>
           </svg>
         </div>
       </div>
-      <ul class="navigationHeader absolute bg-white shadow flex flex-col items-end text-xl top-0 z-50 left-[-500px] w-8/12 h-screen text-right text-gray-800 pt-4 pr-4 font-semibold space-y-8 z-[999] md:relative md:bg-transparent md:text-base md:w-auto md:h-full md:flex-row md:items-start md:shadow-transparent md:space-y-0 md:space-x-12 md:left-0 md:p-0 dark:text-white">
+      <ul class="navigationHeader absolute bg-white shadow flex flex-col items-end text-xl top-0 z-50 left-[-500px] w-8/12 h-screen text-right text-gray-800 pt-4 pr-4 font-semibold space-y-8 z-[999] md:relative md:bg-transparent md:text-base md:w-auto md:h-full md:flex-row md:items-start md:shadow-transparent md:space-y-0 md:space-x-12 md:left-0 md:p-0 dark:bg-primary dark:text-white">
         <!--CROSS-->
         <svg xmlns="http://www.w3.org/2000/svg" @click="disparitionHeader()" class="absolute top-4 left-4 h-8 w-8 md:hidden" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/>
@@ -64,12 +64,14 @@ export default {
 
   methods: {
     apparitionHeader() {
-      document.querySelector(".navigationHeader").classList.toggle("header-mobile");
-      document.querySelector("#overlay").classList.remove("invisible");
+      document.querySelector(".navigationHeader").classList.toggle("header-mobile")
+      document.querySelector(".burger").classList.add("hidden")
+      document.querySelector("#overlay").classList.remove("invisible")
     },
     disparitionHeader() {
-      document.querySelector(".navigationHeader").classList.remove("header-mobile");
-      document.querySelector("#overlay").classList.add("invisible");
+      document.querySelector(".navigationHeader").classList.remove("header-mobile")
+      document.querySelector(".burger").classList.remove("hidden")
+      document.querySelector("#overlay").classList.add("invisible")
     },
 
     nightMode() {
